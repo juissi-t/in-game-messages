5a4cf8e75a7d36634e8072e5670d92b23da8f36a
Juha Tiensyrjä <juha.tiensyrja@gmail.com>
2021-02-08 12:50:12 +0200

    Bump minimum versions of requirements

--- setup.py
+++ setup.py
@@ -30,10 +30,10 @@ setup(
     package_data={},
     install_requires=[
         "typer==0.3.*,>=0.3.2",
-        "setuptools>=51,<54",
+        "setuptools>=53,<54",
         "requests==2.25.*,>=2.25.0",
-        "slack-sdk>=3.1,<3.4",
-        "shellingham>=1.3,<1.5",
+        "slack-sdk>=3.3,<3.4",
+        "shellingham>=1.4,<1.5",
         "wheel==0.36.*,>=0.36.2",
     ],
     extras_require={

d35378826227402f701c02d2b46fba9ac2dbd894
Juha Tiensyrjä <juha.tiensyrja@gmail.com>
2024-10-17 09:41:52 +0300

    chore: Update black and pylint to latest

--- setup.py
+++ setup.py
@@ -47,8 +47,8 @@ setup(
     extras_require={
         "dev": [
             "pytest==8.3.3",
-            "black==24.8.0",
-            "pylint==3.2.7",
+            "black==24.10.0",
+            "pylint==3.3.1",
             "flake8==7.1.1",
             "mypy==1.12.0",
             "types-requests==2.32.0.20240914",
